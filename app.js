a44af9a8086246f319c2b30e771b930edd7f3e61
Alex Romer <aljoromer@gmail.com>
2022-05-05 13:27:26 -0400

    Add .env for env variables, delete unused code

--- app.js
+++ app.js
@@ -13,15 +13,14 @@ const cookieParser = require("cookie-parser"); // to handle cookies
 const session = require("express-session"); // to handle sessions using cookies
 const debug = require("debug")("personalapp:server"); 
 const layouts = require("express-ejs-layouts");
-const axios = require("axios")
 var MongoDBStore = require('connect-mongodb-session')(session);
+require('dotenv').config();
 
 
 // *********************************************************** //
 //  Loading models
 // *********************************************************** //
-const ToDoItem = require("./models/ToDoItem")
-const Overheard = require('./models/Overheard')
+
 const SaleItem = require('./models/SaleItem')
 
 // *********************************************************** //
@@ -30,10 +29,7 @@ const SaleItem = require('./models/SaleItem')
 
 const mongoose = require( 'mongoose' );
 
-//const mongodb_URI = process.env.mongodb_URI
-//const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
-//const mongodb_URI = 'mongodb+srv://cs_sj:BrandeisSpr22@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const mongodb_URI = "mongodb+srv://aljoromer:WlH4NBTrvf8W7Vzm@cluster0.9r8lz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongodb_URI = process.env.mongodb_URI
 
 mongoose.connect( mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
 // fix deprecation warnings
@@ -149,14 +145,13 @@ async (req,res,next) => {
     createAt: new Date()
   })
   await item.save()
-  res.redirect('/saleItem')
+  res.redirect('/')
 })
 
 app.get('/saleItem', 
   async (req,res,next) => {
     const items = await SaleItem.find({})
     res.json(items)
-    //res.render('overheard')
 })
 
 app.get('/forSale',
@@ -166,152 +161,18 @@ app.get('/forSale',
       let userId = res.locals.user._id;  // get the user's id
       let items = await SaleItem.find(); // lookup the user's todo items
       res.locals.items = items;  //make the items available in the view
-      res.render("forSale");  // render to the toDo page
+      res.render("forSale");  // render to the forSale page
     } catch (e){
       next(e);
     }
   }
   )
 
-
-app.get('/overheardForm',
-  (req,res,next) => {
-    res.render('overheardForm')
-  })
-
-app.post('/overheard',
-  isLoggedIn,
-  async (req,res,next) => {
-    const {overheard} = req.body
-    const item = new Overheard({
-      userId: req.session.user._id,
-      comment:overheard,
-      createAt: new Date()
-    })
-    await item.save()
-    res.redirect('/overheard')
-  })
-
-  app.get('/overheard', 
-    async (req,res,next) => {
-      const items = await Overheard.find({})
-      res.json(items)
-      //res.render('overheard')
-    })
 
 app.get("/about", (req, res, next) => {
   res.render("about");
 });
 
-/*
-    ToDoList routes
-*/
-app.get('/todo',
-  isLoggedIn,   // redirect to /login if user is not logged in
-  async (req,res,next) => {
-    try{
-      let userId = res.locals.user._id;  // get the user's id
-      let items = await ToDoItem.find({userId:userId}); // lookup the user's todo items
-      res.locals.items = items;  //make the items available in the view
-      res.render("toDo");  // render to the toDo page
-    } catch (e){
-      next(e);
-    }
-  }
-  )
-
-  app.post('/todo/add',
-  isLoggedIn,
-  async (req,res,next) => {
-    try{
-      const {title,description} = req.body; // get title and description from the body
-      const userId = res.locals.user._id; // get the user's id
-      const createdAt = new Date(); // get the current date/time
-      let data = {title, description, userId, createdAt,} // create the data object
-      let item = new ToDoItem(data) // create the database object (and test the types are correct)
-      await item.save() // save the todo item in the database
-      res.redirect('/todo')  // go back to the todo page
-    } catch (e){
-      next(e);
-    }
-  }
-  )
-
-  app.get("/todo/delete/:itemId",
-    isLoggedIn,
-    async (req,res,next) => {
-      try{
-        const itemId=req.params.itemId; // get the id of the item to delete
-        await ToDoItem.deleteOne({_id:itemId}) // remove that item from the database
-        res.redirect('/todo') // go back to the todo page
-      } catch (e){
-        next(e);
-      }
-    }
-  )
-
-  app.get("/todo/completed/:value/:itemId",
-  isLoggedIn,
-  async (req,res,next) => {
-    try{
-      const itemId=req.params.itemId; // get the id of the item to delete
-      const completed = req.params.value=='true';
-      await ToDoItem.findByIdAndUpdate(itemId,{completed}) // remove that item from the database
-      res.redirect('/todo') // go back to the todo page
-    } catch (e){
-      next(e);
-    }
-  }
-)
-
-/* ************************
-  Functions needed for the course finder routes
-   ************************ */
-
-function getNum(coursenum){
-  // separate out a coursenum 103A into 
-  // a num: 103 and a suffix: A
-  i=0;
-  while (i<coursenum.length && '0'<=coursenum[i] && coursenum[i]<='9'){
-    i=i+1;
-  }
-  return coursenum.slice(0,i);
-}
-
-
-function times2str(times){
-  // convert a course.times object into a list of strings
-  // e.g ["Lecture:Mon,Wed 10:00-10:50","Recitation: Thu 5:00-6:30"]
-  if (!times || times.length==0){
-    return ["not scheduled"]
-  } else {
-    return times.map(x => time2str(x))
-  }
-  
-}
-function min2HourMin(m){
-  // converts minutes since midnight into a time string, e.g.
-  // 605 ==> "10:05"  as 10:00 is 60*10=600 minutes after midnight
-  const hour = Math.floor(m/60);
-  const min = m%60;
-  if (min<10){
-    return `${hour}:0${min}`;
-  }else{
-    return `${hour}:${min}`;
-  }
-}
-
-function time2str(time){
-  // creates a Times string for a lecture or recitation, e.g. 
-  //     "Recitation: Thu 5:00-6:30"
-  const start = time.start
-  const end = time.end
-  const days = time.days
-  const meetingType = time['type'] || "Lecture"
-  const location = time['building'] || ""
-
-  return `${meetingType}: ${days.join(",")}: ${min2HourMin(start)}-${min2HourMin(end)} ${location}`
-}
 
 
 // here we catch 404 errors and forward to error handler
